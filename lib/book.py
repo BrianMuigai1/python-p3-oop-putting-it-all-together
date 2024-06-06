01df22567da9083f52176a59b6b5e42fd132e327
Brian <brian.m.muigai@gmail.com>
2024-06-06 13:58:02 +0300

    feat:completed lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,46 @@
-#!/usr/bin/env python3
+import sys
+import io
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count 
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+            raise ValueError("page_count must be an integer")
+        self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")  
+
+
+
+def test_requires_int_page_count():
+    book = Book("And Then There Were None", 272)
+    captured_out = io.StringIO()
+    sys.stdout = captured_out
+    try:
+        book.page_count = "not an integer"
+    except ValueError as e:
+        pass
+    sys.stdout = sys.__stdout__
+    assert captured_out.getvalue() == "page_count must be an integer\n"
+
+def test_can_turn_page():
+    book = Book("The World According to Garp", 69)
+    captured_out = io.StringIO()
+    sys.stdout = captured_out
+    book.turn_page()
+    sys.stdout = sys.__stdout__
+    assert captured_out.getvalue() == "Flipping the page...wow, you read fast!\n"
+
+
+test_requires_int_page_count()
+test_can_turn_page()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,55 @@
-#!/usr/bin/env python3
+import unittest
+import io
+import sys
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size  # This will use the setter
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+            raise ValueError("size must be an integer")
+        self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+class TestShoe(unittest.TestCase):
+    def test_requires_int_size(self):
+        '''prints "size must be an integer" if size is not an integer.'''
+        stan_smith = Shoe("Adidas", 9)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        try:
+            stan_smith.size = "not an integer"
+        except ValueError:
+            pass
+        finally:
+            sys.stdout = sys.__stdout__
+        self.assertEqual(captured_out.getvalue(), "size must be an integer\n")
+
+    def test_can_cobble(self):
+        '''says that the shoe has been repaired.'''
+        stan_smith = Shoe("Adidas", 9)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        stan_smith.cobble()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_out.getvalue(), "Your shoe is as good as new!\n")
+
+    def test_cobble_makes_new(self):
+        '''creates an attribute on the instance called 'condition' and set equal to 'New' after repair.'''
+        stan_smith = Shoe("Adidas", 9)
+        stan_smith.cobble()
+        self.assertEqual(stan_smith.condition, "New")
+
+if __name__ == '__main__':
+    unittest.main()
